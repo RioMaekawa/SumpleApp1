b0725c281180c8efec54c43472c210a530f8b759
RioMaekawa <bakushishi.ems@gmail.com>
2019-02-12 01:40:32 +0900

    割り算アプリをGithub上で公開する。

---
+++ WindowsForms_DivisionApp/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace WindowsForms_DivisionApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtWari = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNinzu = new System.Windows.Forms.TextBox();
+            this.txtTotalPrice = new System.Windows.Forms.TextBox();
+            this.Resetbtn = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(24, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 14);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "合計金額";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(24, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 14);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "人数";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(26, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 14);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "1人当たり";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(213, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 14);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "円";
+            // 
+            // txtWari
+            // 
+            this.txtWari.Location = new System.Drawing.Point(100, 135);
+            this.txtWari.Name = "txtWari";
+            this.txtWari.ReadOnly = true;
+            this.txtWari.Size = new System.Drawing.Size(99, 19);
+            this.txtWari.TabIndex = 9;
+            this.txtWari.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtWari.TextChanged += new System.EventHandler(this.txtWari_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(213, 99);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 14);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "人";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("MS UI Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label6.Location = new System.Drawing.Point(213, 57);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 14);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "円";
+            // 
+            // txtNinzu
+            // 
+            this.txtNinzu.Location = new System.Drawing.Point(100, 97);
+            this.txtNinzu.Name = "txtNinzu";
+            this.txtNinzu.Size = new System.Drawing.Size(99, 19);
+            this.txtNinzu.TabIndex = 13;
+            this.txtNinzu.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtNinzu.TextChanged += new System.EventHandler(this.txtNinzu_TextChanged);
+            // 
+            // txtTotalPrice
+            // 
+            this.txtTotalPrice.Location = new System.Drawing.Point(100, 55);
+            this.txtTotalPrice.Name = "txtTotalPrice";
+            this.txtTotalPrice.Size = new System.Drawing.Size(99, 19);
+            this.txtTotalPrice.TabIndex = 12;
+            this.txtTotalPrice.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Resetbtn
+            // 
+            this.Resetbtn.Enabled = false;
+            this.Resetbtn.Location = new System.Drawing.Point(112, 201);
+            this.Resetbtn.Name = "Resetbtn";
+            this.Resetbtn.Size = new System.Drawing.Size(75, 23);
+            this.Resetbtn.TabIndex = 17;
+            this.Resetbtn.Text = "リセット";
+            this.Resetbtn.UseVisualStyleBackColor = true;
+            this.Resetbtn.Click += new System.EventHandler(this.Resetbtn_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Enabled = false;
+            this.btnCalculate.Location = new System.Drawing.Point(112, 172);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 16;
+            this.btnCalculate.Text = "計算";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 260);
+            this.Controls.Add(this.Resetbtn);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtNinzu);
+            this.Controls.Add(this.txtTotalPrice);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtWari);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "割り算アプリ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtWari;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtNinzu;
+        private System.Windows.Forms.TextBox txtTotalPrice;
+        private System.Windows.Forms.Button Resetbtn;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ WindowsForms_DivisionApp/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_DivisionApp
+{
+    public partial class Form1 : Form
+    {
+        private string before_TotalPrice;
+        private string before_Ninzu;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //初期起動時、ボタンは非活性
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnCalculate.Enabled = false;
+            Resetbtn.Enabled = false;
+        }
+
+
+        //計算ボタンのイベント処理
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //入力された文字を数字に変換
+            int sumPrice = int.Parse(txtTotalPrice.Text);
+            int Ninzu = int.Parse(txtNinzu.Text);
+
+            //割り算結果の数字を文字に変換し出力
+            txtWari.Text = (sumPrice / Ninzu).ToString();
+
+            Resetbtn.Enabled = true;
+        }
+
+        //合計金額テキストのイベント処理
+        private void txtWari_TextChanged(object sender, EventArgs e)
+        {
+            int i = 0;
+
+            //合計金額入力時、合計金額テキストボックスに入力データをセット
+            if (int.TryParse(txtTotalPrice.Text, out i) == false && (txtTotalPrice.Text != ""))
+            {
+
+                txtTotalPrice.Text = before_TotalPrice;
+            }
+
+            before_TotalPrice = txtTotalPrice.Text;
+
+            btnaction();
+        }
+
+        //計算ボタンの活性・非活性化のイベント処理
+        private void btnaction()
+        {
+            //合計金額および人数テキストが入力済の場合、計算ボタンを活性化
+            if (txtTotalPrice.Text != "" && txtNinzu.Text != "")
+            {
+                btnCalculate.Enabled = true;
+            }
+            else
+            {
+                btnCalculate.Enabled = false;
+            }
+
+        }
+
+        //人数テキストのイベント処理
+        private void txtNinzu_TextChanged(object sender, EventArgs e)
+        {
+            int i = 0;
+
+            //人数入力時、人数テキストボックスに入力データをセット
+            if (int.TryParse(txtNinzu.Text, out i) == false && (txtNinzu.Text != ""))
+            {
+                txtNinzu.Text = before_Ninzu;
+            }
+
+            before_Ninzu = txtNinzu.Text;
+
+            btnaction();
+        }
+
+        //リセットボタンのイベント処理
+        private void Resetbtn_Click(object sender, EventArgs e)
+        {
+            txtTotalPrice.Clear();
+            txtNinzu.Clear();
+            txtWari.Text = String.Empty;
+
+            Resetbtn.Enabled = false;
+        }
+    }
+}
